dd9a3353fb9391cdf7b72efd986d629c6f779d06
HongLinh <vohonglinh229@gmail.com>
2015-11-17 11:29:28 +0700

    iCookAPI
    Initial

---
+++ WebAPI/iCookAPI/src/main/java/com/cathl/service/DataService.java
@@ -0,0 +1,12 @@
+package com.cathl.service;
+
+import java.util.List;
+
+import com.cathl.model.User;
+/**
+ * @author LinhVH
+ *
+ */
+public interface DataService {
+	public List<User> getUserList();
+}
