6c7f673d9de07351c12c4ffc22cb51ed7fa5acd9
HongLinh <vohonglinh229@gmail.com>
2015-11-27 13:13:12 +0700

    iCookAPI
    Updated

--- WebAPI/iCookAPI/src/main/java/com/cathl/icookapi/controller/FoodController.java
+++ WebAPI/iCookAPI/src/main/java/com/cathl/icookapi/controller/FoodController.java
@@ -4,12 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.cathl.icookapi.dto.FoodDTO;
 import com.cathl.icookapi.entity.Food;
