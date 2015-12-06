79bf1f4e7578367bd5a0dc6d66b6fdc522066cd8
HongLinh <vohonglinh229@gmail.com>
2015-12-06 14:36:52 +0700

    Remove unused

--- WebApp/iCook/src/main/java/com/cathl/icook/dao/FoodDAO.java
+++
@@ -1,7 +0,0 @@
-package com.cathl.icook.dao;
-
-import com.cathl.icook.entity.Food;
-
-public interface FoodDAO {
-	public Food getFood(int id);
-}
--- WebApp/iCook/src/main/java/com/cathl/icook/service/FoodService.java
+++
@@ -1,8 +0,0 @@
-package com.cathl.icook.service;
-
-import com.cathl.icook.entity.Food;
-
-public interface FoodService {
-
-	public Food getFood(int id);
-}
