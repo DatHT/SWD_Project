130ac925d1cea3d77fcf87126d61c3a57ec9b869
HongLinh <vohonglinh229@gmail.com>
2015-12-06 16:27:32 +0700

    [LinhVH]Update security

--- WebAPI/iCookAPI/src/main/java/com/cathl/icookapi/controller/FoodController.java
+++ WebAPI/iCookAPI/src/main/java/com/cathl/icookapi/controller/FoodController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cathl.icookapi.dto.FoodDTO;
+import com.cathl.icookapi.dto.FoodDetailDTO;
 import com.cathl.icookapi.entity.Food;
+import com.cathl.icookapi.entity.FoodDetail;
+import com.cathl.icookapi.service.FoodDetailService;
 import com.cathl.icookapi.service.FoodService;
 
 @RestController
@@ -21,6 +24,8 @@ public class FoodController {
 	
 	@Autowired
 	private FoodService foodService;
+//	@Autowired
+//	private FoodDetailService foodDetailService;
 	
 	@RequestMapping("/food/{id}")
 	public Food getFood(@PathVariable("id") int foodId) {
@@ -32,12 +37,18 @@ public class FoodController {
 	@RequestMapping("search/{materials}/{start}/{limit}")
 	public List<FoodDTO> searchByMaterial(@PathVariable("materials") String materials,
 			@PathVariable("start") int start, @PathVariable("limit") int limit) {
-		List<Food> listFood;
-		listFood = foodService.searchByMaterial(materials, start, limit);
+		List<Food> listFood = foodService.searchByMaterial(materials, start, limit);
 		List<FoodDTO> listFoodDTO = new ArrayList<FoodDTO>();
 		for (Food food : listFood) {
 			listFoodDTO.add(new FoodDTO(food));
 		}
 		return listFoodDTO;
 	}
+//	
+//	@RequestMapping("search/detail/{id}")
+//	public FoodDetailDTO getFoodDetail(@PathVariable("id") int id) {
+//		FoodDetail foodDetail = foodDetailService.getFood(id);
+//		FoodDetailDTO foodDetailDTO = new FoodDetailDTO(foodDetail);
+//		return foodDetailDTO;
+//	}
 }
